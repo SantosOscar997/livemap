82754c6d8f492cf8c84f473b324e3b87e495e1a7
rzavalet <rj.zavaleta@gmail.com>
2021-12-12 23:00:53 -0600

    Fixed Leaflet usage and Kafka usage

---
+++ required_setup.md
@@ -0,0 +1,3 @@
+Antes de correr el servidor web y el productor, hay que iniciar una instancia
+de Kafka. La forma mas sencilla es seguir los pasos (1) y (2) en
+https://kafka.apache.org/quickstart.
--- static/leaf.js
+++ static/leaf.js
@@ -1,16 +1,20 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'ABCDEFG' //Acceso al TOKEN
-}).addTo(mymap);
+	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+	var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+
 
 mapMarkers1 = [];
 mapMarkers2 = [];
 mapMarkers3 = [];
 
-var source = new EventSource('/topic/TOPICNAME'); //Genera una conexión con el servidor
+var source = new EventSource('/topic/geodata_final123'); //Genera una conexión con el servidor
 source.addEventListener('message', function(e){
 
   console.log('Message');
